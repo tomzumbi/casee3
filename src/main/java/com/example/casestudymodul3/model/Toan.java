a02d7dfe9ad02c672479e3571cfb018657c34144
conPuppy <hoangnhatnam29102000@gmail.com>
2022-12-29 15:40:23 +0700

    doneFund

---
+++ src/main/java/com/example/casestudymodul3/model/Toan.java
@@ -0,0 +1,5 @@
+package com.example.casestudymodul3.model;
+
+public class Toan {
+
+}
---
+++ src/main/java/com/example/casestudymodul3/model/hiep.java
@@ -0,0 +1,5 @@
+package com.example.casestudymodul3.model;
+
+public class hiep {
+    private String name;
+}
